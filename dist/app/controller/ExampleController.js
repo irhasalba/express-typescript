07fd8f53f9c1b1e43f6956e0f1862e11824bf4c7
irhasalba <irhas@fit.id>
2022-10-13 12:42:11 +0700

    delete dist directory

--- dist/app/controller/ExampleController.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class ExampleController {
-    index(req, res) {
-        res.send({ message: "OK" });
-    }
-}
-exports.default = new ExampleController();
